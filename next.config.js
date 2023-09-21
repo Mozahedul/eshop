0ded7ac61ea695696342765e1891824ac878e151
Mozahedul <mozahed001@gmail.com>
2023-09-21 15:23:04 +0600

    Change domain name in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['http://localhost:3000'],
+    domains: ['https://eshop-fawn.vercel.app/'],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96],
     remotePatterns: [
