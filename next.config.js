ebb79d75086037bec9d7878bef4c5832281f2506
Mozahedul <mozahed001@gmail.com>
2023-10-04 20:12:03 +0600

    Add domain to next.config.js file

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    // domains: ['https://eshop-fawn.vercel.app/'],
+    domains: ['https://sparkly-halva-73cb5e.netlify.app/'],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96],
     remotePatterns: [
