70d378a5ddb93fd8131d0e7f53886ad0c628145b
Mozahedul <mozahed001@gmail.com>
2023-10-07 22:02:14 +0600

    Error in add to cart button resolved

--- utils/functions/AddToCart.js
+++ utils/functions/AddToCart.js
@@ -2,19 +2,11 @@ import axios from 'axios';
 
 // Add product to cart
 const addToCartHandle = async (product, dispatch, qty, router) => {
-  // for handling the error of request cancelling by user
-  // taking more time to complete the request
-  // prevent unnecessary network trafficking
-  const source = axios.CancelToken.source();
-
   try {
-    const response = await axios(
-      {
-        method: 'get',
-        url: `/api/products/${product._id}`,
-      },
-      { cancelToken: source.token }
-    );
+    const response = await axios({
+      method: 'get',
+      url: `/api/products/${product._id}`,
+    });
 
     if (response.statusText === 'OK') {
       dispatch({
@@ -23,19 +15,9 @@ const addToCartHandle = async (product, dispatch, qty, router) => {
       });
 
       router.push('/cart');
-      // to cancel the completion of request takes more than 2 seconds
-      setTimeout(function () {
-        source.cancel('Request cancelled by user');
-      }, 2000);
-    } else {
-      throw new Error('Something went wrong on the server');
     }
   } catch (error) {
-    if (axios.isCancel(error)) {
-      console.log('Request cancelled ==> ', error.message);
-    } else {
-      console.log('Error without axios ==> ', error.message);
-    }
+    console.log('Request cancelled ==> ', error.message);
   }
   return true;
 };
