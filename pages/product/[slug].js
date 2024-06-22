40bc2a1e48574e81776aacec4cd976297f05d23e
Mozahedul <mozahed001@gmail.com>
2024-06-22 11:08:08 +0600

    modified:   pages/product/[slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -97,10 +97,10 @@ const ProductScreen = props => {
     //   if (response.status === 200) {
     console.log('Dispatched the products ==> ', { product, quantity: qty });
 
-    dispatch({
-      type: 'CART_ITEM_ADDED',
-      payload: { ...product, quantity: qty },
-    });
+    // dispatch({
+    //   type: 'CART_ITEM_ADDED',
+    //   payload: { ...product, quantity: qty },
+    // });
 
     console.log('navigate to the cart page');
     router.push('/cart');
